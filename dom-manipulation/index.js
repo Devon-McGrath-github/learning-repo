237983a35c95e01cf0453298fbe1f2c4466a358e
Devon McGrath <devon.mcgrath@gmail.com>
2023-04-18 09:16:35 +1200

    Add event listener and multi-select

--- dom-manipulation/index.js
+++ dom-manipulation/index.js
@@ -33,3 +33,23 @@ para4.appendChild(node4);
 div.appendChild(para3)
 div.appendChild(para4)
 container.appendChild(div)
+
+
+const btn = document.querySelector('#btn'); 
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
