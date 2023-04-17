bded97adc76c8b3607a1b427cadcfdc04438d9bf
Devon McGrath <devon.mcgrath@gmail.com>
2023-04-17 16:56:32 +1200

    rushed DOM manipulation

--- dom-manipulation/index.js
+++ dom-manipulation/index.js
@@ -5,20 +5,31 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
-const para = document.createElement('p');
-const paragraph1 = document.createTextNode('Hey I\'m Red!');
-para.appendChild(paragraph1);
-
-
-
-
-// redText.classList.add('redText');
-// redText.textContent = 'Hey Im red!'
-// redText.style.color = 'color: red;';
-
-// const blueText = document.createElement('h3');
-// blueText.classList.add('blueText');
-// blueText.textContent = 'I\'m a blue h3';
-
-
 container.appendChild(content);
+
+const para = document.createElement('p');
+const node = document.createTextNode('I\'m red!');
+para.appendChild(node);
+para.setAttribute('style', 'color: red;')
+
+
+container.appendChild(para)
+
+const para2 = document.createElement('h3');
+const node2 = document.createTextNode('I\'m a blue h3');
+para2.appendChild(node2);
+para2.setAttribute('style', 'color: blue;')
+
+container.appendChild(para2)
+
+const div = document.createElement('div');
+const para3 = document.createElement('h1');
+const node3 = document.createTextNode('I\'m in a div');
+para3.appendChild(node3);
+const para4 = document.createElement('p');
+const node4 = document.createTextNode('Me Too');
+para4.appendChild(node4);
+
+div.appendChild(para3)
+div.appendChild(para4)
+container.appendChild(div)
