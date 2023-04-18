dd2e0fc83893531000a9db7e8c0b115183605830
Devon McGrath <devon.mcgrath@gmail.com>
2023-04-19 08:31:45 +1200

    Add leetCode practice

--- dom-manipulation/index.js
+++ dom-manipulation/index.js
@@ -1,55 +1,14 @@
-// your JavaScript file
-const container = document.querySelector('#container');
+function mergeAlternately(word1, word2) {
+  let length1 = word1.length;
+  let length2 = word2.length;
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+  let result = '';
 
-container.appendChild(content);
+  for (let i = 0; i < Math.max(length1, length2) ; i++) {
+    result += word1.charAt(i);
+    result += word2.charAt(i);
+  }
+  return result;
+};
 
-const para = document.createElement('p');
-const node = document.createTextNode('I\'m red!');
-para.appendChild(node);
-para.setAttribute('style', 'color: red;')
-
-
-container.appendChild(para)
-
-const para2 = document.createElement('h3');
-const node2 = document.createTextNode('I\'m a blue h3');
-para2.appendChild(node2);
-para2.setAttribute('style', 'color: blue;')
-
-container.appendChild(para2)
-
-const div = document.createElement('div');
-const para3 = document.createElement('h1');
-const node3 = document.createTextNode('I\'m in a div');
-para3.appendChild(node3);
-const para4 = document.createElement('p');
-const node4 = document.createTextNode('Me Too');
-para4.appendChild(node4);
-
-div.appendChild(para3)
-div.appendChild(para4)
-container.appendChild(div)
-
-
-const btn = document.querySelector('#btn'); 
-
-btn.addEventListener('click', function (e) {
-  e.target.style.background = 'blue';
-});
-
-
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.id);
-  });
-});
+console.log(mergeAlternately('welcomehome this','ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'))
