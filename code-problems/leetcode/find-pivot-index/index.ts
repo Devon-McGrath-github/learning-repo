e0a9f005c57a79fa99d044759fbe862f340a6788
Devon McGrath <devon.mcgrath@gmail.com>
2023-04-21 16:01:03 +1200

    Refactored find pivot index for slight efficiency gain

--- code-problems/leetcode/find-pivot-index/index.ts
+++ code-problems/leetcode/find-pivot-index/index.ts
@@ -1,16 +1,18 @@
 function pivotIndex(nums: number[]) {
-  let sumLeft: number = 0;
-  let sumRight: number = 0;
+  let sumLeft: number = 0
+  let sumRight: number = 0
+
+  nums.forEach(e => sumRight += e)
 
   let i: number = 0;
   while (i < nums.length) {
     sumLeft = nums.slice(0, i).reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-    sumRight = nums.slice(i+1).reduce((accumulator, currentValue) => accumulator + currentValue, 0)  
-    if (sumLeft === sumRight) return i;
+    sumRight -= nums[i]  
+    if (sumLeft === sumRight) return i
     i++
   }
 
-  return -1;
+  return -1
 };
 
 console.log(pivotIndex([2,1,-1]))
