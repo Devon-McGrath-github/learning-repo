f8e28e91c89a2676769f3c3c917305c48d2dfa55
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-29 15:36:21 +1200

    add start of library app

---
+++ Javascript/jsLibrary/index.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function displayBook() {
+  myLibrary.forEach((element) => {
+    myLibrary.map;
+  });
+}
