5053c77f02d11318c7768d81c9640d8d55885bac
Devon McGrath <devon.mcgrath@gmail.com>
2023-06-30 13:19:36 +1200

    Complete Question 3

--- Recursion/index.js
+++ Recursion/index.js
@@ -5,10 +5,7 @@ https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
 */
 
 function sumRange(n, total = 0) {
-  if (n <= 0) {
-    console.log(total);
-    return total;
-  }
+  if (n <= 0) return total;
   sumRange(n - 1, total + n);
 }
 
@@ -16,3 +13,9 @@ function power(base, exponent) {
   if (exponent === 0) return 1;
   return base * power(base, exponent - 1);
 }
+
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
