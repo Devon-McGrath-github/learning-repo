23ff9a39e1f7274fd0332efc0cbac657ad076908
Devon McGrath <devon.mcgrath@gmail.com>
2023-06-30 13:07:40 +1200

    Add recursive function examples

---
+++ Recursion/index.js
@@ -0,0 +1,12 @@
+function sumRange(n, total = 0) {
+  if (n <= 0) {
+    console.log(total);
+    return total;
+  }
+  sumRange(n - 1, total + n);
+}
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
