ed5dc7018ca925441eaa5cada640ab5bc2838ed9
Devon McGrath <devon.mcgrath@gmail.com>
2023-06-30 13:51:38 +1200

    Complete Question 4

--- Recursion/index.js
+++ Recursion/index.js
@@ -4,18 +4,34 @@ https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
 
 */
 
+// Question 1:
 function sumRange(n, total = 0) {
   if (n <= 0) return total;
   sumRange(n - 1, total + n);
 }
 
+// Question 2:
 function power(base, exponent) {
   if (exponent === 0) return 1;
   return base * power(base, exponent - 1);
 }
-
+// Question 3:
 function factorial(n) {
   if (n === 1) return 1;
   return n * factorial(n - 1);
 }
 
+// Question 4:
+function all(array, callback) {
+  if (array.length <= 0) return true;
+  return callback(array[0]) ? all(array.slice(1), callback) : false;
+}
+
+const exampleCallback = (num) => {
+  return num < 7;
+};
+
+console.log("Should return false: " + all([1, 2, 9], exampleCallback));
+console.log("Should return true: " + all([1, 2, 3], exampleCallback));
+console.log("Should return true: " + all([], exampleCallback));
+console.log("Should return false: " + all([8], exampleCallback));
