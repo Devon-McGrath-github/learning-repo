680ee749ab964c1f20111e555f1a54532ce56e96
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-05 13:40:25 +1200

    Update app following assignment
    https://www.geeksforgeeks.org/reactjs-props-set-1/?ref=lbp

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+
+function DemoComponent(props) {
+  return (
+    <div>
+      {/*accessing information from props */}
+      <h2>Hello {props.user}</h2>
+      <h3>Welcome to GeeksforGeeks</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DemoComponent user="Devon McGrath" />
     </div>
   );
 }
