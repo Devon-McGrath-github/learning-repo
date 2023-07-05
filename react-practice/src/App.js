2bdf7dd7caf102ae376d785f6359305e9164da18
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-05 14:49:40 +1200

    React tasks list example

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,23 +1,55 @@
-import "./App.css";
+import React, { Component } from "react";
+import uniqid from "uniqid";
+import Overview from "./components/Overview";
 
-import React from "react";
+class App extends Component {
+  constructor() {
+    super();
 
-function DemoComponent(props) {
-  return (
-    <div>
-      {/*accessing information from props */}
-      <h2>Hello {props.user}</h2>
-      <h3>Welcome to GeeksforGeeks</h3>
-    </div>
-  );
-}
+    this.state = {
+      task: { text: "", id: uniqid() },
+      tasks: [],
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
 
-function App() {
-  return (
-    <div>
-      <DemoComponent user="Devon McGrath" />
-    </div>
-  );
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview tasks={tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
