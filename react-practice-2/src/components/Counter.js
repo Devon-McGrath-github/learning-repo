4976c985359a6ba5c5eb7742bcf9c2e14d8ee450
Devon McGrath <devon.mcgrath@gmail.com>
2023-07-06 16:08:23 +1200

    Setup simple counter

--- react-practice-2/src/components/Counter.js
+++ react-practice-2/src/components/Counter.js
@@ -1,9 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
   render() {
+    console.log("Render");
+
     return (
-      <div>Counter</div>
-    )
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <h1 className="counter">Counter: {this.state.counter}</h1>
+      </div>
+    );
   }
 }
