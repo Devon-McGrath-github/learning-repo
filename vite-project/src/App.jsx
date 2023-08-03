fe120571834c42a9b13b17fef1bbff17f4163912
Devon McGrath <devon.mcgrath@gmail.com>
2023-08-03 15:40:00 +1200

    basic vite count button + odin project exercise

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [count, setCount] = useState(0);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+    setCount(count + 1);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? 'selected' : ''}
+        >
+          {color}
+        </button>
+      ))}
+      <h1>{count}</h1>
+    </div>
+  );
+}
+
+export default App;
